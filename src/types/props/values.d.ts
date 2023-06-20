df54814adee2adcd8a54c40a131ac2388db6b377
Markus Wendorf <markuswendorf@web.de>
2023-06-20 12:58:00 +0200

    handle required prop

--- src/types/props/values.d.ts
+++ src/types/props/values.d.ts
@@ -2,33 +2,40 @@ import { Field } from '../field';
 import { SelfSource } from '../source';
 
 /** Values based on the fields defined by the component schema */
-export type FieldValue<T extends Field> = {
-  text: string;
-  textarea: string;
-  markdown: string;
-  richtext: unknown;
-  number: number;
-  datetime: string;
-  boolean: boolean;
-  options: Array<InferOptionValue<T>>;
-  option: InferOptionValue<T>;
-  asset: Asset;
-  multiasset: Asset[];
-  multilink: Link;
-  table: Table;
-  custom: never;
-  section: never;
-  tab: never;
-  /**
-   * Field "bloks" types cannot be inferred (could be anything).
-   * The user can extend the inferred type to include the types for nested bloks.
-   */
-  bloks: never;
-}[T['type']];
+export type FieldValue<T extends Field> = Required<
+  T,
+  {
+    text: string;
+    textarea: string;
+    markdown: string;
+    richtext: unknown;
+    number: number;
+    datetime: string;
+    boolean: boolean;
+    options: Array<InferOptionValue<T>>;
+    option: InferOptionValue<T>;
+    asset: Asset;
+    multiasset: Asset[];
+    multilink: Link;
+    table: Table;
+    custom: never;
+    section: never;
+    tab: never;
+    /**
+     * Field "bloks" types cannot be inferred (could be anything).
+     * The user can extend the inferred type to include the types for nested bloks.
+     */
+    bloks: never;
+  }[T['type']]
+>;
 
 type InferOptionValue<T extends Field> = T extends SelfSource
   ? T['options'][number]['value']
   : string;
+
+type Required<T extends Field, Value> = T['required'] extends true
+  ? Value
+  : Value | null | undefined;
 
 export interface Asset {
   fieldtype: 'asset';
